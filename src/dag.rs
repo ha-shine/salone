e6815244642e118695143fe498081eec86be4ba4
Htet Aung Shine <h@shine.rocks>
2020-03-24 00:05:40 +0800

    Refactor to use Arcs in GADDAG similar to the paper

--- src/dag.rs
+++ src/dag.rs
@@ -8,47 +8,54 @@ enum Kind {
     Delim,
 }
 
-struct Node {
-    // The set of letters which, if encountered next, make a word
+struct Arc {
     letter_set: HashSet<char>,
+    next: Node,
+}
 
+impl Arc {
+    fn new() -> Self {
+        Arc {
+            letter_set: HashSet::new(),
+            next: Node::new(),
+        }
+    }
+
+    fn add_word<'a>(&mut self, kind: &Kind, mut words: Peekable<impl Iterator<Item=&'a Kind>>) {
+        let peek = words.peek();
+
+        match (kind, peek) {
+            (Kind::Char(ch), None) => {
+                self.letter_set.insert(*ch);
+            },
+            _ => {
+                self.next.add_word(words);
+            }
+        }
+    }
+}
+
+struct Node {
     // Arcs going out from this node, associated with the letter
-    arcs: HashMap<Kind, Node>,
+    arcs: HashMap<Kind, Arc>,
 }
 
 impl Node {
     fn new() -> Self {
         Node {
-            letter_set: HashSet::new(),
             arcs: HashMap::new(),
         }
     }
 
     fn add_word<'a>(&mut self, mut words: Peekable<impl Iterator<Item=&'a Kind>>) {
         let next = words.next().unwrap();
-        let peek = words.peek();
 
-        match peek {
-            None => {
-                self.add_final(next)
-            }
-
-            Some(_) => {
-                if let None = self.arcs.get(next) {
-                    self.arcs.insert(*next, Self::new());
-                }
-
-                let mut next_state = self.arcs.get_mut(next).unwrap();
-                next_state.add_word(words);
-            }
+        if let None = self.arcs.get(next) {
+            self.arcs.insert(*next, Arc::new());
         }
-    }
 
-    fn add_final(&mut self, kind: &Kind) {
-        match kind {
-            Kind::Char(c) => { self.letter_set.insert(*c); }
-            _ => {}
-        }
+        let mut arc = self.arcs.get_mut(next).unwrap();
+        arc.add_word(next, words);
     }
 }
 
